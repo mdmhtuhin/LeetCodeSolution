2b13fdf0e63a5cc740fa3d9a6edec41d71e1d9ab
MD Mehedi Hasan Tuhin <tuhinh02@gmail.com>
2025-12-23 13:45:04 +0600

    Time: 0 ms (60.00%), Space: 227.83 MB (80.00%) - LeetSnap

--- Medium/3784-Minimum-Deletion-Cost-to-Make-All-Characters-Equal/Solution.rb
+++ Medium/3784-Minimum-Deletion-Cost-to-Make-All-Characters-Equal/Solution.rb
@@ -1,19 +1,15 @@
-# Time: 304 ms (20.00%), Space: 231.10 MB (20.00%)
+# Time: 0 ms (60.00%), Space: 227.83 MB (80.00%)
 
 # @param {String} s
 # @param {Integer[]} cost
 # @return {Integer}
 def min_cost(s, cost)
-    min_cost = Float::INFINITY
     total_cost = cost.sum
     cost_of_chars = Hash.new(total_cost)
     s.each_char.with_index do |ch, index|
         cost_of_chars[ch] -= cost[index]
     end
 
-    cost_of_chars.each_value do |v|
-        min_cost = v if v < min_cost
-    end
-    min_cost
+    cost_of_chars.
     
 end
