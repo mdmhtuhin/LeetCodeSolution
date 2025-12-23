0ef3ad0b8d3285535791dc6eb90740daf19aa192
MD Mehedi Hasan Tuhin <tuhinh02@gmail.com>
2025-12-23 14:58:17 +0600

    Add solution for Remove Element (Easy) - LeetSnap

--- Easy/0027-Remove-Element/Solution.js
+++ Easy/0027-Remove-Element/Solution.js
@@ -1,10 +1,21 @@
-// Time: 72 ms (3.65%), Space: 211 MB (100.00%)
-
 /**
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    
+    if (nums.length === 0) return 0;
+
+  let k = 0;
+  let i = 0;
+
+  while (i < nums.length) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+    i++;
+  }
+
+  return k;
 };
