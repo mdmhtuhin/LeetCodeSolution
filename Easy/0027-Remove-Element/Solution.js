e8ba7a02970fa4e785818736626d0825cbff4e31
MD Mehedi Hasan Tuhin <tuhinh02@gmail.com>
2025-12-23 14:56:10 +0600

    Time: 72 ms (3.65%), Space: 211 MB (100.00%) - LeetSnap

---
+++ Easy/0027-Remove-Element/Solution.js
@@ -0,0 +1,10 @@
+// Time: 72 ms (3.65%), Space: 211 MB (100.00%)
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    
+};
