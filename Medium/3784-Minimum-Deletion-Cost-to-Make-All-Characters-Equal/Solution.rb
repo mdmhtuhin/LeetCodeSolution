40d5e6b7391914a896f069c9f5933b40794b04b0
MD Mehedi Hasan Tuhin <tuhinh02@gmail.com>
2025-12-23 13:35:34 +0600

    Minimum Deletion Cost to Make All Characters Equal (Medium) - LeetSnap

---
+++ Medium/3784-Minimum-Deletion-Cost-to-Make-All-Characters-Equal/Solution.rb
@@ -0,0 +1,24 @@
+# @param {String} s
+# @param {Integer[]} cost
+# @return {Integer}
+def min_cost(s, cost)
+    min_cost = Float::INFINITY
+    cost_of_chars = {}
+    i = 0
+    total_cost = cost.sum
+    s.split("").each_with_index do |ch, index|
+        if cost_of_chars.keys.include?(ch)
+            cost_of_chars[ch] -= cost[index]
+        else
+            cost_of_chars[ch] = total_cost - cost[index]
+        end
+    end
+
+    cost_of_chars.each do |k, v|
+        if v < min_cost
+            min_cost = v
+        end
+    end
+    min_cost
+    
+end
