1203e7cf572ef1e33d4bf985d3698a87e30a8c8b
MD Mehedi Hasan Tuhin <tuhinh02@gmail.com>
2025-12-23 14:59:59 +0600

    Add solution for Remove Element (Easy) - LeetSnap

--- Easy/0027-Remove-Element/Solution.rb
+++ Easy/0027-Remove-Element/Solution.rb
@@ -1,8 +1,16 @@
-# Time: 0 ms (%), Space: 53.6 MB (%)
-
 # @param {Integer[]} nums
 # @param {Integer} val
 # @return {Integer}
 def remove_element(nums, val)
-    
+    return 0 if nums.length == 0
+    k = 0
+    i = 0
+    while i < nums.length
+        if nums[i] != val
+            nums[k] = nums[i]
+            k += 1
+        end
+        i += 1
+    end
+    k
 end
