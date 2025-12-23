b43b8121c1f553462400d071fce97ad23f9e1572
MD Mehedi Hasan Tuhin <tuhinh02@gmail.com>
2025-12-23 14:34:59 +0600

    Time: 0 ms (%), Space:  (%) - LeetSnap

---
+++ Medium/0011-Container-With-Most-Water/Solution.rb
@@ -0,0 +1,25 @@
+# Time: 0 ms (%), Space:  (%)
+
+# @param {Integer[]} height
+# @return {Integer}
+def max_area(height)
+    start = 0
+    last = height.length - 1
+    area = 0
+    while start < last
+        s = height[start]
+        l = height[last]
+        if (s <= l )&& (area < s * (last - start))
+            area = s * (last - start)
+        elsif (s >= l) && (area < l * (last - start))
+            area = l * (last - start)
+        end
+        if s <= l
+            start += 1
+        elsif s >= l
+            last -= 1
+        end
+    end
+    area
+        
+end
