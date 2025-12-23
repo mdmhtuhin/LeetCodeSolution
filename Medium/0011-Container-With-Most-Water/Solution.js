e8955dbf43e0eb8f9b5e8957481504f9e2d0a541
MD Mehedi Hasan Tuhin <tuhinh02@gmail.com>
2025-12-23 14:47:56 +0600

    Add solution for Container With Most Water (Medium) - LeetSnap

--- Medium/0011-Container-With-Most-Water/Solution.js
+++ Medium/0011-Container-With-Most-Water/Solution.js
@@ -1,9 +1,29 @@
-// Time: 50 ms (15.10%), Space: 218.20 MB (19.18%)
-
 /**
  * @param {number[]} height
  * @return {number}
  */
 var maxArea = function(height) {
-    
+    let start = 0;
+  let last = height.length - 1;
+  let area = 0;
+
+  while (start < last) {
+    const s = height[start];
+    const l = height[last];
+    const width = last - start;
+
+    if (s <= l && area < s * width) {
+      area = s * width;
+    } else if (s >= l && area < l * width) {
+      area = l * width;
+    }
+
+    if (s <= l) {
+      start++;
+    } else {
+      last--;
+    }
+  }
+
+  return area;
 };
