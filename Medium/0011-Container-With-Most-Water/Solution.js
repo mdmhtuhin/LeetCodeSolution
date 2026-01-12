cd5ba88ebdd7094272b3684840a29b1c39a8382e
MD Mehedi Hasan Tuhin <tuhinh02@gmail.com>
2026-01-13 03:53:18 +0600

    Time: 3 ms (51.23870000000001%), Space: 63.1 MB (67.1611%) - LeetSnap

---
+++ Medium/0011-Container-With-Most-Water/Solution.js
@@ -0,0 +1,31 @@
+// Time: 3 ms (51.23870000000001%), Space: 63.1 MB (67.1611%)
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let start = 0;
+  let last = height.length - 1;
+  let area = 0;
+
+  while (start < last) {
+    const s = height[start];
+    const l = height[last];
+    const width = last - start;
+
+    if (s <= l && area < s * width) {
+      area = s * width;
+    } else if (s >= l && area < l * width) {
+      area = l * width;
+    }
+
+    if (s <= l) {
+      start++;
+    } else {
+      last--;
+    }
+  }
+
+  return area;
+};
